e8b9ca18c7fe5b46bf90da060366568f5adb7828
albertJ32 <00b0d14c@opayq.com>
2015-08-06 21:18:45 +0100

    initial commit

---
+++ build/CMakeFiles/multiDimen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/multiDimen.dir/multiDimen.cpp.o"
+  "multiDimen.pdb"
+  "multiDimen"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/multiDimen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
