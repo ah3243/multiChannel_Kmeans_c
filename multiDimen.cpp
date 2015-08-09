363251a9117cc1e40b988f503f9a74440424bdb8
albertJ32 <00b0d14c@opayq.com>
2015-08-09 22:33:09 +0100

    toggle generate and save textonDictionary, basic model creation with rudimentary extractor must convert to BoWimgDescriptionExtractor

--- multiDimen.cpp
+++ multiDimen.cpp
@@ -1,13 +1,12 @@
-//Goals//
-// 1. import and save 200x200pixel image in Mat
-// 2. Segment image into 20 10x10pixel images and store in Mat's
-// 3. apply basic gaussian blur
-// 4. apply kmeans to blured Segments, store in vector
-// 5. repeat 1-4 with image from second class
+///////////////////////////////////////
+// Opencv BoW Texture classification //
+///////////////////////////////////////
 
 #include "opencv2/highgui/highgui.hpp"
 #include <opencv2/opencv.hpp>
 #include "opencv2/core/core.hpp"
+#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/legacy/legacy.hpp>
 //#include "features2d.hpp" // For feature2d (typedef DescriptorExtractor)
 #include <iostream>
 
@@ -23,11 +22,14 @@
 #include <sys/types.h>
 #include <algorithm> // Maybe fix DescriptorExtractor doesn't have a member 'create'
 
+#define DICTIONARY_BUILD 0
 
 using std::vector;
 using namespace cv;
 using namespace std;
 
+typedef vector<Mat> m;
+typedef vector<m> mV;
 
 void listDir(const char *inPath, vector<Mat>& dirFiles){
   DIR *pdir = NULL;
@@ -51,8 +53,9 @@ void listDir(const char *inPath, vector<Mat>& dirFiles){
     ss << pent->d_name;
     string a = ss.str();
     Mat tmp = imread(a, CV_BGR2GRAY);
-    dirFiles.push_back(tmp);
-    if(!tmp.data){
+    if(tmp.data){
+      dirFiles.push_back(tmp);
+    }else{
       cout << "unable to read image.." << endl;
     }
   }
@@ -60,22 +63,15 @@ void listDir(const char *inPath, vector<Mat>& dirFiles){
   cout << "finished Reading Successfully.." << endl;
 }
 
-int main( int /*argc*/, char** /*argv*/ )
-{
+void importImgs(mV &modelImg, vector<string> classes){
   string basePath = "../../../TEST_IMAGES/kth-tips/";
-  vector<string> extPath = {"bread/", "cotton/", "cork/"};
-  typedef vector<Mat> mV;
-  mV txtons(100, Mat(200,200, CV_32FC1));
-  mV bread (10,Mat(200,200,CV_32FC1));
-  mV cotton (10, Mat(200,200,CV_32FC1));
-  mV cork (10, Mat(200,200,CV_32FC1));
 
   int count = 0;
-  for(int i=0;i<4;i++){
+  for(int i=0;i<classes.size();i++){
     stringstream ss;
     ss << basePath;
-    ss << extPath[count];
-    ss << "train/";
+    ss << classes[count];
+    ss << "/train/";
     string b = ss.str();
     const char* a = b.c_str();
     cout << "this is the path.. " << a << endl;
@@ -83,33 +79,63 @@ int main( int /*argc*/, char** /*argv*/ )
     switch(i){
       case 0:
       cout << "number: " << i << endl;
-      listDir("../../../TEST_IMAGES/kth-tips/textons/",txtons);
-      count--;
+      listDir(a, modelImg[0]);
       break;
 
       case 1:
       cout << "number: " << i << endl;
-      listDir(a,bread);
+      listDir(a, modelImg[1]);
       break;
 
       case 2:
       cout << "number: " << i << endl;
-      listDir(a,cotton);
-      break;
-
-      case 3:
-      cout << "number: " << i << endl;
-      listDir(a,cork);
+      listDir(a, modelImg[2]);
       break;
     }
     count ++;
   }
-  cout << "This is the size of textons: " << txtons.size() << ", bread: " << bread.size() << ", cotton: " << cotton.size() << ", cork: " << cork.size() << endl;
+  cout << "This is the size of bread: " << modelImg[0].size() << ", cotton: " << modelImg[1].size() << ", cork: " << modelImg[2].size() << endl;
+}
 
+// int main(int argc, char** argv){
+  //   if(argc<3){
+  //     cout << "not enough inputs entered, exiting." << endl;
+  //     exit(1);
+  //   }
+  //   Mat img1 = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+  //   Mat img2 = imread(argv[2], CV_LOAD_IMAGE_GRAYSCALE);
+  //   if(img1.empty() || img2.empty())
+  //   {
+  //       printf("Can't read one of the images\n");
+  //       return -1;
+  //   }
+  //
+  //   // detecting keypoints
+  //   SurfFeatureDetector detector(400);
+  //   vector<KeyPoint> keypoints1, keypoints2;
+  //   detector.detect(img1, keypoints1);
+  //   detector.detect(img2, keypoints2);
+  //
+  //   // computing descriptors
+  //   SurfDescriptorExtractor extractor;
+  //   Mat descriptors1, descriptors2;
+  //   extractor.compute(img1, keypoints1, descriptors1);
+  //   extractor.compute(img2, keypoints2, descriptors2);
+  //
+  //   // matching descriptors
+  //   BruteForceMatcher<L2<float> > matcher;
+  //   vector<DMatch> matches;
+  //   matcher.match(descriptors1, descriptors2, matches);
+  // drawing the results
+  //  namedWindow("matches", 1);
+  //  Mat img_matches;
+  //  drawMatches(img1, keypoints1, img2, keypoints2, matches, img_matches);
+  //  imshow("matches", img_matches);
+  //  waitKey(0);
+  //}
 
-  Mat in = imread("../../bread.png", CV_BGR2GRAY);
+Mat reshapeCol(Mat in){
   Mat points(in.rows*in.cols, 1,CV_32F);
-
   int cnt = 0;
   for(int i =0;i<in.rows;i++){
     for(int j=0;j<in.cols;j++){
@@ -117,188 +143,124 @@ int main( int /*argc*/, char** /*argv*/ )
       cnt++;
     }
   }
+  return points;
+}
 
-  int dictSize = 100;
+
+int main( int /*argc*/, char** /*argv*/ ){
+  mV modelImg(3, m(10, Mat(200,200,CV_32FC1)));
+  vector<string> classes = {"bread", "cotton", "cork"};
+
+  importImgs(modelImg, classes);
+
+  //////////////////////////////
+  // Create Texton vocabulary //
+  //////////////////////////////
+  #if DICTIONARY_BUILD == 1
+  int dictSize = 28;
   int attempts = 1;
   int flags = KMEANS_PP_CENTERS;
   TermCriteria tc(TermCriteria::MAX_ITER + TermCriteria::EPS, 10, 0.001);
 
-
   BOWKMeansTrainer bowTrainer(dictSize, tc, attempts, flags);
 
-  bowTrainer.add(points);
-  bowTrainer.add(points);
-  bowTrainer.add(points);
-//  cout << "this is the size of the local Feature set: " << bowTrainer.descriptorsCount() << endl;
+  vector<Mat> txtons;
+  listDir("../../../TEST_IMAGES/kth-tips/textons/",txtons);
+
+  for(int i=0;i<txtons.size();i++){
+    Mat points = reshapeCol(txtons[i]);
+    cout << "\n\ntHis is the size: " << points.rows << endl;
+    cout << "loop number: " << i << endl;
+    bowTrainer.add(points);
+    cout << "bowTrainer.discriptorCount: " << bowTrainer.descripotorsCount() << endl;
+  }
   Mat dictionary = bowTrainer.cluster();
   cout << "This is the dictionary size: " << dictionary.size() << endl;
 
+  FileStorage fs("dictionary.xml",FileStorage::WRITE);
+  fs << "vocabulary" << dictionary;
+  fs.release();
 
-  // Create Models/Train Classifier
+
+  #else
+  ///////////////////////////////////////////////////////////
+  // Get histogram responses using vocabulary from Classes //
+  ///////////////////////////////////////////////////////////
+
   vector<KeyPoint> keypoints;
   Mat response_hist;
+  vector<string> class_names;
   Mat img;
   string filepath;
   map<string,Mat> classes_training_data;
 
-  Ptr<FeatureDetector> detector = FeatureDetector::create("SURF");
-  Ptr<DescriptorExtractor> discriptor = DescriptorExtractor::create("SIFT");
-  Ptr<cv::DescriptorMatcher> matcher = DescriptorMatcher::create("FlannBased");
-  BOWImgDescriptorExtractor bowDE(discriptor, matcher);
+  vector<Mat> dictionary;
+  FileStorage fs("dictionary.xml",FileStorage::READ);
+  fs["vocabulary"] >> dictionary;
+  fs.release();
 
-  bowDE.setVocabulary(dictionary);
+  Ptr<FeatureDetector> detector = FeatureDetector::create("FAST");
+  SurfDescriptorExtractor extractor;
+  // Ptr<DescriptorExtractor> discriptor = DescriptorExtractor::create("SURF");
+  Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("FlannBased");
 
-  img = imread("../../bread.png", CV_BGR2GRAY);
-  //detector.detect(img, keypoints);
-  bowDE.compute(img, keypoints, response_hist);
+  // BOWImgDescriptorExtractor bowDE(discriptor, matcher);
+
+  //  bowDE.setVocabulary(dictionary);
+  cout << "\n\n\nCreate Models: \n" ;
+
+  for(int i = 0; i < modelImg.size(); i++){
+    for(int j = 0; j < modelImg[i].size(); j++){
+      Mat bob = imread("../../bread.png", CV_BGR2GRAY);;
+      detector->detect(bob, keypoints);
+
+      cout << "blah.. i: " << i << " j: " << j << endl;
+      extractor.compute(bob, keypoints, response_hist);
+      // bowDE.compute(bob, keypoints, response_hist);
+
+      if(!classes_training_data.count(classes[i])){
+        classes_training_data[classes[i]].create(0,response_hist.cols,response_hist.type());
+        class_names.push_back(classes[i]);
+      }
+      classes_training_data[classes[i]].push_back(response_hist);
+    }
+  }
 
 
+  //////////////////////////////////////////
+  // Create 1 against all SVM classifiers //
+  //////////////////////////////////////////
+
+  // for(int i=0;i<classes.size();i++){
+  //   string class_ = classes[i];
+  //   cout << "Currently training class: " << class_ << endl;
+  //
+  //   Mat samples(0, response_Hist.cols, response_Hist.type());
+  //   Mat labels(0, 1, CV_32FC1);
+  //
+  //   // Copy class samples and labels
+  //   cout << "adding " << classes_training_data[class_].rows << " positive" << endl;
+  //   samples.push_back(classes_training_data[class_]);
+  //   Mat class_label = Mat::ones(classes_training_data[class_].rows, 1, CV_32FC1);
+  //   labels.push_back(class_label);
+  //
+  //   for (map<string,Mat>::iterator it1 = classes_training_data.begin(); it1 != classes_training_data.end(); ++it1) {
+  //         string not_class_ = (*it1).first;
+  //         if(not_class_.compare(class_)==0) continue; //skip class itself
+  //         samples.push_back(classes_training_data[not_class_]);
+  //         class_label = Mat::zeros(classes_training_data[not_class_].rows, 1, CV_32FC1);
+  //         labels.push_back(class_label);
+  //      }
+  //
+  //      cout << "Train.." << endl;
+  //      Mat samples_32f; samples.convertTo(samples_32f, CV_32F);
+  //      if(samples.rows == 0) continue; //phantom class?!
+  //      CvSVM classifier;
+  //      classifier.train(samples_32f,labels);
+  //
+  //      // Save the classifier to file or cache
+  // }
+  #endif
 
   return 0;
 }
-
-//  cout << "\n Usage in C++ API:\n double kmeans(const Mat& samples, int clusterCount, Mat& labels, TermCriteria termcrit, int attempts, int flags, Mat* centers) \n\n\n" << endl;
-//
-//  int clusterCount = 2;
-//  int dimensions = 2;
-//  int sampleCount = 20*20;
-//
-//  Mat points(sampleCount,dimensions, CV_32FC1,Scalar(10));
-//  Mat in =imread("../../10pixsq.png", CV_BGR2GRAY);
-//  Mat labels;
-//  Mat centers(clusterCount, 1, points.type());
-//
-//
-// // // values of 1st half of data set is set to 10
-// // // change the values of 2nd half of the data set; i.e. set it to 20
-//
-// int cnt = 0; // To make sure img vals are stored from 0
-//  for(int i =0;i<in.rows;i++)
-//  {
-//   for(int j=0;j<in.cols;j++)
-//   {
-//       points.at<float>(cnt,0) = in.at<Vec3b>(i,j)[0];
-// //      points.at<float>(cnt,1) = in.at<Vec3b>(i,j)[0];
-//       points.at<float>(cnt,1)=rand()%100;
-//     cnt++;
-//   }
-//  }
-//
-//
-// //Mat out = points.reshape(1, points.rows*points.cols);
-//
-// kmeans(points, clusterCount, labels, TermCriteria( CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, 10, 1.0), 3, KMEANS_PP_CENTERS, centers);
-//     // we can print the matrix directly.
-//  cout<<"Data: \n"<<points<<endl;
-//  cout << "Size: " << points.size() << endl;
-//  cout << "This has .channels(): " << points.channels() << endl;
-// cout<<"Center: \n"<<centers<<endl;
-// //cout<<"Labels: \n"<<labels<<endl;
-//  return 0;
-// }
-
-/// ---------------------------------------------------------------------------------------------------- ///
-
-// #include "opencv2/imgproc/imgproc.hpp"
-// #include "opencv2/highgui/highgui.hpp"
-// #include <iostream>
-// #include <boost/math/special_functions/round.hpp>
-// #include <boost/filesystem.hpp>
-// #include <boost/range/algorithm.hpp>
-//
-// using namespace boost::filesystem;
-// using namespace cv;
-// using namespace std;
-//
-// void printCluster(Mat centers){
-//     int r = centers.rows;
-//     int c = centers.cols;
-//
-//     cout << "This is the cluster size inside printing: " << centers.size() << endl;
-//     for(int i=0;i<c;i++){
-//       cout << "This is the col: " << i << endl;
-//       for(int j=0;j<r;j++){
-//         cout << centers.at<float>(i+j*c) << ", i:" << j << endl;
-//       }
-//       cout << "Looping.. " << endl;
-//     }
-//     cout << "Leaving.. " << endl;
-// }
-//
-// void fillImg(Mat in, Mat& out, int channel){
-//   int r = in.rows;
-//   int c = in.cols;
-//
-//   for(int i=0;i<c;i++){
-//     for(int j=0;j<r;j++){
-//       out.at<float>((j+i*c),channel) = in.at<float>(i,j);
-// //      cout << "value: " << (j+i*c) << endl;
-//     }
-//   }
-// }
-//
-// void segmentImg(Mat in, Mat& segImg){
-//   int chan = 0;
-//   int rmax =8;
-//   int cmax =1;
-//
-//   for(int c=0; c < cmax; c++){
-//     for(int r=0 ;r < rmax; r++){
-//       // Select ROI and copy to tmp Matrix
-//       Mat tmp;
-//       in(Rect(r,c,20,20)).copyTo(tmp);
-//
-//       fillImg(tmp,segImg,chan);
-//       cout << "Val: " << segImg.at<Vec3f>(r,c)[0]  << " chan: " << chan<< endl;
-//       chan++;
-//     }
-//   }
-// }
-//
-// int main( int argc, char** argv )
-// {
-//   string basePath = "../../../TEST_IMAGES/kth-tips/bread/train/";
-//   path p(basePath);
-// //    getImages(p);
-//   stringstream ss;
-//   ss << basePath;
-//   ss << "52a-scale_2_im_1_col.png";
-//   Mat input1 = imread(ss.str(), CV_BGR2GRAY);
-//   cout << "This is the img input size: " << input1.size() << endl;
-
-//  int segSide = 20;
-//  Mat segImgArr = Mat::zeros(segSide*segSide, 8, CV_32FC2);
-//  segmentImg(input1, segImgArr);
-  // cout << "This is the size of the Mat going in: " << segImgArr.size() << " rows: " << segImgArr.rows << " cols: " << segImgArr.cols << endl;
-  //
-  // Mat labels(1000,1000, CV_8U);
-  // Mat centers;
-  // int attempts = 5, clustercnt = 2;
-  // kmeans(segImgArr, clustercnt, labels, TermCriteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, 10000, 0.0001), attempts, KMEANS_PP_CENTERS, centers);
-  //
-  //   cout << "This is the size of the labels: " << labels.size() << " Rows: " << labels.rows << " Cols: " << labels.cols << endl;
-  //   cout << "labels: " << labels << endl;
-  //   cout << "CEntroids: " << centers << endl;
-
-/// -------------------------------------------------------------------------------------------------------- ///
-  // Mat src = imread( argv[1], 1 );
-  // Mat samples(src.rows * src.cols, 1, CV_32FC(1));
-  // for( int y = 0; y < src.rows; y++ )
-  //   for( int x = 0; x < src.cols; x++ )
-  //     for( int z = 0; z < 3; z++)
-  //       samples.at<Vec3f>(y + x*src.rows, 1)[0] = src.at<Vec3b>(y,x)[z]; // Multichannel array going into a single channel Mat
-  //
-  // int clusterCount = 5;
-  // Mat labels;
-  // int attempts = 5;
-  // Mat centers;
-  // kmeans(samples, clusterCount, labels, TermCriteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, 10000, 0.0001), attempts, KMEANS_PP_CENTERS, centers );
-  //
-  // cout << "centers size: " << centers.size() << endl;
-  // cout << "centers: " << endl;
-  // cout << centers ;
-  // cout << endl;
-  //
-  //  cout << "labels size: " << labels.size() << endl;
-  // waitKey( 0 );
-//}
