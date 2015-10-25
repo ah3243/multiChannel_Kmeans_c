3cd3937b657b02a1f3067157c4e8dc35af1ca3c4
albertJ32 <00b0d14c@opayq.com>
2015-10-25 01:08:30 +0100

    Removed testing module consolidated it with ar drone code.

--- singleImgEval.h
+++
@@ -1,8 +0,0 @@
-// Single Image Evaluation Module
-
-#ifndef singleImgEval
-#define singleImgEval
-
-  int directionHandle(std::string inImg, std::map<std::string, int> params, std::map<std::string, double> paramsDB, std::string goal);
-
-#endif
